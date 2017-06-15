c15271ba26aa1e7d885cb6ebdc2ee534e16771a9
manideeppaturu <mp672@njit.edu>
2017-06-14 18:22:18 -0700

    Add files via upload

---
+++ 2.ValidateUserEntriesMonolithic/src/validateuserentriesmonolithic/ValidateUserEntriesMonolithic.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validateuserentriesmonolithic;
+
+/**
+ *
+ * @author Manideep
+ */
+public class ValidateUserEntriesMonolithic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ValidateUserEntries().setVisible(true);
+            }
+        });
+    }
+    
+}
